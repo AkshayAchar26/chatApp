f13bc6ce7287df7423e58d521b7d44d40b907703
Akshay <akshaymanjunath26@gmail.com>
2024-05-14 17:36:40 +0530

    build: initial setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
+  rules: {
+    "no-console": ["error"],
+    "no-alert": ["error"],
+    "no-unused-vars": ["error"],
+    "prefer-arrow-callback": ["error"],
+    "max-lines-per-function": ["error", 100],
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+  },
+};
