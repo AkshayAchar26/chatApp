3510bb40a6f460a46ab020d93af23d8d00d919a9
Akshay <akshay.m@briskthought.com>
2024-05-16 20:02:47 +0530

    feat: set up husky and commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-empty": [2, "never"],
+    "type-enum": [
+      2,
+      "always",
+      ["ci", "feat", "fix", "refactor", "revert", "style"],
+    ],
+  },
+};
